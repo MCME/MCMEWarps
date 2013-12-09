483ba0e653ba99c8b10b31d6029fed20ca559b34
Henry Slawniak <meggawatts@gmail.com>
2013-12-08 23:58:13 -0600

    Warp searching part 2

---
+++ src/main/java/co/mcme/warps/commands/WarpSearchCommand.java
@@ -0,0 +1,161 @@
+/*  This file is part of MCMEWarps.
+ * 
+ *  MCMEWarps is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  MCMEWarps is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with MCMEWarps.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package co.mcme.warps.commands;
+
+import co.mcme.warps.Warps;
+import co.mcme.warps.storage.PlayerWarp;
+import co.mcme.warps.storage.SearchWarps;
+import co.mcme.warps.storage.SearchWarps.SearchResult;
+import co.mcme.warps.storage.WarpDatabase;
+import java.util.ArrayList;
+import java.util.Date;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.util.ChatPaginator;
+
+public class WarpSearchCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("You must be a player to run this command.");
+            return true;
+        }
+        Player player = (Player) sender;
+        if (args.length > 0) {
+            String search = args[0];
+            if (search.toLowerCase().contains("@")) {
+                Warps.getServerInstance().getLogger().info(search);
+                search = search.replaceAll("@", "");
+                Warps.getServerInstance().getLogger().info(search);
+                if (WarpDatabase.getWarpCreators().containsKey(search)) {
+                    ArrayList<PlayerWarp> warps = WarpDatabase.getWarpCreators().get(search).getWarps();
+                    StringBuilder customList = new StringBuilder();
+                    boolean first = true;
+                    for (PlayerWarp warp : warps) {
+                        if (!first) {
+                            customList.append("\n");
+                        }
+                        ChatColor col = ChatColor.GREEN;
+                        if (warp.isInviteonly()) {
+                            col = ChatColor.RED;
+                        }
+                        Date date = new Date(warp.getCreateStamp());
+                        customList.append(col).append(warp.getName())
+                                .append(ChatColor.GRAY).append(" created by ")
+                                .append(ChatColor.AQUA).append(warp.getOwner())
+                                .append(ChatColor.GRAY).append(" on ")
+                                .append(ChatColor.AQUA).append(Warps.getDateformat().format(date));
+                        if (first) {
+                            first = false;
+                        }
+                    }
+                    if (args.length > 1) {
+                        ChatPaginator.ChatPage page = ChatPaginator.paginate(customList.toString(), Integer.valueOf(args[1]), ChatPaginator.AVERAGE_CHAT_PAGE_WIDTH, 8);
+                        sender.sendMessage(ChatColor.GRAY + search + "'s warps " + ChatColor.AQUA + page.getPageNumber() + ChatColor.GRAY + " of " + ChatColor.AQUA + page.getTotalPages());
+                        for (String line : page.getLines()) {
+                            sender.sendMessage(line);
+                        }
+                        return true;
+                    } else {
+                        ChatPaginator.ChatPage page = ChatPaginator.paginate(customList.toString(), 1, ChatPaginator.AVERAGE_CHAT_PAGE_WIDTH, 8);
+                        sender.sendMessage(ChatColor.GRAY + search + "'s warps " + ChatColor.AQUA + page.getPageNumber() + ChatColor.GRAY + " of " + ChatColor.AQUA + page.getTotalPages());
+                        for (String line : page.getLines()) {
+                            sender.sendMessage(line);
+                        }
+                        return true;
+                    }
+                } else {
+                    sender.sendMessage(ChatColor.RED + "Cannot find any warps by " + search);
+                    return true;
+                }
+            }
+            SearchResult res = SearchWarps.searchWarps(search, player);
+            StringBuilder exactLines = new StringBuilder();
+            boolean first = true;
+            for (PlayerWarp warp : res.getExact()) {
+                if (!first) {
+                    exactLines.append("\n");
+                }
+                ChatColor col = ChatColor.GREEN;
+                if (warp.isInviteonly()) {
+                    col = ChatColor.RED;
+                }
+                Date date = new Date(warp.getCreateStamp());
+                exactLines.append(col).append(warp.getName())
+                        .append(ChatColor.GRAY).append(" created by ")
+                        .append(ChatColor.AQUA).append(warp.getOwner())
+                        .append(ChatColor.GRAY).append(" on ")
+                        .append(ChatColor.AQUA).append(Warps.getDateformat().format(date));
+                if (first) {
+                    first = false;
+                }
+            }
+
+            StringBuilder partialLines = new StringBuilder();
+            first = true;
+            for (PlayerWarp warp : res.getPartial()) {
+                if (!first) {
+                    partialLines.append("\n");
+                }
+                ChatColor col = ChatColor.GREEN;
+                if (warp.isInviteonly()) {
+                    col = ChatColor.RED;
+                }
+                Date date = new Date(warp.getCreateStamp());
+                partialLines.append(col).append(warp.getName())
+                        .append(ChatColor.GRAY).append(" created by ")
+                        .append(ChatColor.AQUA).append(warp.getOwner())
+                        .append(ChatColor.GRAY).append(" on ")
+                        .append(ChatColor.AQUA).append(Warps.getDateformat().format(date));
+                if (first) {
+                    first = false;
+                }
+            }
+            if (res.getExact().size() > 0) {
+                ChatPaginator.ChatPage page = ChatPaginator.paginate(partialLines.toString(), 1, ChatPaginator.AVERAGE_CHAT_PAGE_WIDTH, 8);
+                sender.sendMessage(ChatColor.GRAY + "Exact Matches for " + args[0]);
+                for (String line : page.getLines()) {
+                    sender.sendMessage(line);
+                }
+                return true;
+            }
+            if (res.getPartial().size() > 0) {
+                if (args.length > 1) {
+                    ChatPaginator.ChatPage page = ChatPaginator.paginate(partialLines.toString(), Integer.valueOf(args[1]), ChatPaginator.AVERAGE_CHAT_PAGE_WIDTH, 8);
+                    sender.sendMessage(ChatColor.GRAY + "Partial Matches page " + ChatColor.AQUA + page.getPageNumber() + ChatColor.GRAY + " of " + ChatColor.AQUA + page.getTotalPages());
+                    for (String line : page.getLines()) {
+                        sender.sendMessage(line);
+                    }
+                    return true;
+                } else {
+                    ChatPaginator.ChatPage page = ChatPaginator.paginate(partialLines.toString(), 1, ChatPaginator.AVERAGE_CHAT_PAGE_WIDTH, 8);
+                    sender.sendMessage(ChatColor.GRAY + "Partial Matches page " + ChatColor.AQUA + page.getPageNumber() + ChatColor.GRAY + " of " + ChatColor.AQUA + page.getTotalPages());
+                    for (String line : page.getLines()) {
+                        sender.sendMessage(line);
+                    }
+                    return true;
+                }
+            }
+            sender.sendMessage(ChatColor.RED + "No warps found for " + args[0]);
+            return true;
+        }
+        return false;
+    }
+}
---
+++ src/main/java/co/mcme/warps/storage/SearchWarps.java
@@ -0,0 +1,64 @@
+/*  This file is part of MCMEWarps.
+ * 
+ *  MCMEWarps is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  MCMEWarps is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with MCMEWarps.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package co.mcme.warps.storage;
+
+import java.util.ArrayList;
+import java.util.List;
+import lombok.Getter;
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
+
+public class SearchWarps {
+
+    public static SearchResult searchWarps(String search, OfflinePlayer player) {
+        ArrayList<PlayerWarp> exact = new ArrayList();
+        ArrayList<PlayerWarp> partial = new ArrayList();
+        List<String> names = new ArrayList(WarpDatabase.getWarps().keySet());
+
+        for (String wName : names) {
+            PlayerWarp warp = WarpDatabase.getWarp(wName);
+            if (warp.canWarp(player)) {
+                if (warp.getName().equalsIgnoreCase(search)) {
+                    exact.add(warp);
+                } else if (warp.getName().toLowerCase().contains(search.toLowerCase())) {
+                    partial.add(warp);
+                }
+            }
+        }
+        for (PlayerWarp warp : partial) {
+            if (exact.contains(warp)) {
+                exact.remove(warp);
+            }
+        }
+        Bukkit.getLogger().info(search);
+        Bukkit.getLogger().info(exact.toString());
+        Bukkit.getLogger().info(partial.toString());
+        return new SearchResult(exact, partial);
+    }
+
+    public static class SearchResult {
+
+        public SearchResult(ArrayList<PlayerWarp> exact, ArrayList<PlayerWarp> partial) {
+            this.exact = exact;
+            this.partial = partial;
+        }
+
+        @Getter
+        private final ArrayList<PlayerWarp> exact;
+        @Getter
+        private final ArrayList<PlayerWarp> partial;
+    }
+}
